f2a29cc5560fd04c1e1c12f4a9b5ce0e0c03a44d
Devon Schimming <xnomadsix@gmail.com>
2018-11-02 15:59:32 -0500

    started hw05 queue

---
+++ hw05 - queue/src/Queue.hpp
@@ -0,0 +1,34 @@
+#ifndef QUEUE__HPP
+#define QUEUE__HPP
+#include <iostream>
+#include <utility>
+
+template <typename T>
+class Queue {
+public:
+    // Default Constructor
+    // Parameterized Constructor
+    // Copy Constructor
+    // Move Constructor
+    // Destructor
+    // function empty, does not throw exceptions
+    // function size, does not throw exceptions
+    // function front, l-value, throws underflow
+    // function front, read-only, throws underflow
+    // function back, l-value, throws underflow
+    // function back, read-only, throws underflow
+    // function push, does not throw exceptions
+    // function emplace, does not throw exceptions
+    // function pop, throws underflow
+    // Copy assignment operator overload
+    // Move assignment operator overload
+private:
+    // Private data / underlying container
+};
+
+
+
+// QUEUE IMPLEMENTATION
+
+
+#endif
